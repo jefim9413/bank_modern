bf15425180ca683f0186b443539761436e1de3c4
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-01 14:46:39 -0300

    iniciando Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import styles from './style';
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello, World</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
     </div>
-  )
+);
+
 export default App
