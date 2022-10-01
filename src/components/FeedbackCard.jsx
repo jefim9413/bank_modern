cb93ec8c47b8a11263e3da6511a0f41e4c872db9
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-01 15:43:38 -0300

    iniciando o componente FeedbackCard

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class FeedbackCard extends Component {
+  render() {
+    return (
+      <div>FeedbackCard</div>
+    )
+  }
+}
+
+export default FeedbackCard
